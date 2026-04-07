a3652e8822212705f70401cbd19ef5689a110947
GuiSamSamTei <guilhermesampaio365@gmail.com>
2026-04-07 13:26:47 +0200

    feat: initial springboot project

---
+++ transpitrack/src/main/java/fr/utc/miage/transpitrack/TranspitrackApplication.java
@@ -0,0 +1,13 @@
+package fr.utc.miage.transpitrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranspitrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranspitrackApplication.class, args);
+	}
+
+}
---
+++ transpitrack/src/test/java/fr/utc/miage/transpitrack/TranspitrackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.utc.miage.transpitrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranspitrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
