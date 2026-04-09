40c5d1c1e714356137c20c4544f343635009b5f7
walaedinesekoub <walaedine.sekoubterminal@gmail.com>
2026-04-09 16:05:55 +0200

    feat : Creation Modification information personnelle

--- src/main/java/fr/utc/miage/transpitrack/Controller/UserController.java
+++ src/main/java/fr/utc/miage/transpitrack/Controller/UserController.java
@@ -1,19 +1,18 @@
 package fr.utc.miage.transpitrack.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import fr.utc.miage.transpitrack.Model.User;
-import fr.utc.miage.transpitrack.Model.Jpa.UserService;
-import jakarta.servlet.http.HttpSession;
-
-import org.springframework.web.bind.annotation.PostMapping;
-
 import fr.utc.miage.transpitrack.Model.Enum.Gender;
+import fr.utc.miage.transpitrack.Model.Jpa.UserService;
+import fr.utc.miage.transpitrack.Model.User;
+import jakarta.servlet.http.HttpSession;
 
 
 @Controller
@@ -22,6 +21,8 @@ public class UserController {
    
     @Autowired
     UserService userService;
+
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     @GetMapping("/formCreate")
     public String formCreate(@RequestParam(required=false) String message,
@@ -89,5 +90,101 @@ public class UserController {
         //TODO : à modifier à l'avenir quand la page sera définie
         return "dashboard";
     }
+
+
+    @GetMapping("/formUpdate")
+    public String formUpdate(@RequestParam(required=false) String message,
+        Model model, 
+        HttpSession session){
+
+        Long userId = (Long) session.getAttribute("userId");
+
+        if(userId==null){
+            //TODO : à modifier à l'avenir quand la page sera définie
+           return "formLogin";
+        }
+        User user = userService.getUserById(userId);
+        model.addAttribute("message", message);
+        model.addAttribute("user", user);
+
+        return "formUpdate";
+    }
+
+    @PostMapping("/updateUser")
+    public String updateUser(@RequestParam("firstName") String firstName, 
+                            @RequestParam("name") String name,
+                            @RequestParam("email") String email,
+                            @RequestParam("password") String password,
+                            @RequestParam("age") int age, 
+                            @RequestParam("height") double height,
+                            @RequestParam("gender") String gender,
+                            @RequestParam("weight") double weight,
+                            @RequestParam("city") String city, 
+                            Model model,
+                            HttpSession session) {
+
+        if(!email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$")){
+            model.addAttribute("message", "Email n'est pas au bon format");
+            return "formUpdate";
+        }
+                                
+        if(age<0){
+            model.addAttribute("message", "Age ne peut pas être négatif");
+            return "formUpdate";
+        }
+        if(height<0){
+            model.addAttribute("message", "Taille ne peut pas être négatif");
+            return "formUpdate";
+        }
+
+        if(weight<0){
+            model.addAttribute("message", "Poids ne peut pas être négatif");
+            return "formUpdate";
+        }
+
+        Long actualUserId = (Long) session.getAttribute("userId");
+
+        User actualUser = userService.getUserById(actualUserId);
+
+        if(!actualUser.getEmail().equals(email)){
+            User userExist = userService.getUserByEmail(email);
+
+            if(userExist==null){
+                actualUser.setName(name);
+                actualUser.setFirstName(firstName);
+                actualUser.setAge(age);
+                actualUser.setGender(Gender.valueOf(gender));
+                actualUser.setEmail(email);
+                if(!password.isBlank()){
+                    actualUser.setPassword(encoder.encode(password));
+                }
+                actualUser.setHeight(height);
+                actualUser.setWeight(weight);
+                actualUser.setCity(city);
+            }else{
+                model.addAttribute("message", "email déja existant");
+                return "formUpdate";
+            }
+        }else{
+                actualUser.setName(name);
+                actualUser.setFirstName(firstName);
+                actualUser.setAge(age);
+                actualUser.setGender(Gender.valueOf(gender));
+                if(!password.isBlank()){
+                    actualUser.setPassword(encoder.encode(password));
+                }
+                actualUser.setHeight(height);
+                actualUser.setWeight(weight);
+                actualUser.setCity(city);
+        }
+
+        userService.updateUser(actualUser);
+        model.addAttribute("message", "Modification du compte réussie");
+
+        //TODO : à modifier à l'avenir quand la page "profil" sera définie
+        return "dashboard";
+    }
+
+
     
 }
